5d1950df64407b457ee845b64ff8aa65342fa13c
VINCENT ADENIJI <54925113+TOSINNIJIS1@users.noreply.github.com>
2020-09-08 23:05:49 -0400

    classes and more

---
+++ src/drawing_shape.py
@@ -0,0 +1,21 @@
+# Drawing Shape
+print('   /|')
+print('  / |')
+print(' /  |')
+print('/___|')
+
+
+# Variables 
+
+char_name = "Vincent ", "Tosin "
+char_lname = "Adeniji"
+char_sch = "Lambda Academy"
+char_major = "FullStack Web Development"
+
+print("Hello " + char_name[1] + char_lname + ", ")
+print("You're Currently Studying " + char_major)
+print("at " + char_sch)
+
+# Working With String 
+print('Vincent Adeniji') 
+print('Vincent \nAdeniji')  # Line Break
